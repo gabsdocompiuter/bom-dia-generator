8dd56015e256f94a95b55fabf77e6beea59ad03d
Gabs Monteiro <gmonteeeiro@gmail.com>
2021-05-26 19:36:50 -0300

    Create a bot to organizze assets

--- bom-dia-generator/bots/organizze_assets.py
+++ bom-dia-generator/bots/organizze_assets.py
@@ -9,7 +9,9 @@ def read_dir(dir):
             if entry.is_dir():
                 read_dir(entry.path)
             else:
-                os.rename(entry.path, f"{dir}/{image_counter}.jpg")
+                _, extension = os.path.splitext(entry.path)
+                os.rename(entry.path, f"{dir}/{image_counter}{extension}")
+                
                 image_counter += 1
 
 path = pathlib.Path().absolute()
@@ -17,4 +19,3 @@ assets_dir = f"{path}/../assets/"
 
 
 read_dir(assets_dir)
-# print(assets_dir)
