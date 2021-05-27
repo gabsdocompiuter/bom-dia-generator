7f1d84ba5ea5ff55f39bceb49d119b24b90ffd97
Gabs Monteiro <gmonteeeiro@gmail.com>
2021-05-27 09:28:41 -0300

    Add resize_image method

--- bom-dia-generator/helpers/image_helper.py
+++ bom-dia-generator/helpers/image_helper.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import textwrap
 
 class ImageHelper:
     def __init__(self):
@@ -10,23 +11,50 @@ class ImageHelper:
         blur_image = original_image.filter(ImageFilter.BoxBlur(blur_perc))
         return blur_image
 
-    def cut_image(self, width, height):
-        pass
+    def resize_image(self, image, width, height):
+        image_width, image_height = image.size
+
+        left = (image_width - width) / 2
+        right = (image_width + width) / 2
+        top = (image_height - height) / 2
+        bottom = (image_height + height) / 2
+
+        if left < 0:
+            left = 0
+        
+        if top < 0:
+            top = 0
+
+        if right > (image_width - left):
+            right = image_width - left
+
+        if bottom > (image_height - top):
+            bottom = image_height - top
+
+        print((left, top, right, bottom))
+
+        return image.crop((left, top, right, bottom))
 
     def add_text(self, image, font_asset, message):
         image_draw = ImageDraw.Draw(image)
         image_font = ImageFont.truetype(font_asset, 80)
 
         width, height = image.size
-        text_width, text_height = image_draw.textsize(message, font=image_font)
 
+        #decide a cor do texto
         rgb_image = image.convert('RGB')
         r, g, b = rgb_image.getpixel((width / 2, height / 2))
-        print(f'{r}, {g}, {b}')
         text_color = (255 - r, 255 - g, 255 - b)
 
-        position_x = (width  - text_width)  / 2
-        position_y = (height - text_height) / 2
+        lines = textwrap.wrap(message, width=30)
+        position_y = height / 3
 
-        image_draw.text((position_x, position_y), message, font=image_font, fill=text_color)
+        for line in lines:
+            text_width, text_height = image_draw.textsize(line, font=image_font)
+
+            position_x = (width  - text_width)  / 2
+            
+            image_draw.text((position_x, position_y), line, font=image_font, fill=text_color)
+            position_y += text_height
+
         image.show()
--- bom-dia-generator/tests/blur_image.py
+++ bom-dia-generator/tests/blur_image.py
@@ -6,8 +6,9 @@ from helpers.image_helper import ImageHelper
 image_helper = ImageHelper()
 
 assets_dir = "D:/github/bom-dia-generator/bom-dia-generator/assets/"
-image_asset = f"{assets_dir}/images/backgrounds/2.jpg"
+image_asset = f"{assets_dir}/images/backgrounds/3.jpg"
 font_asset = f"{assets_dir}/fonts/0.ttf"
 
 blur_image = image_helper.blur_image(image_asset, 5)
-final_image = image_helper.add_text(blur_image, font_asset, 'BOM DIA, KKKKKKK')
+resize_image = image_helper.resize_image(blur_image, 1024, 1024)
+final_image = image_helper.add_text(resize_image, font_asset, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tristique ligula vitae molestie suscipit.')
